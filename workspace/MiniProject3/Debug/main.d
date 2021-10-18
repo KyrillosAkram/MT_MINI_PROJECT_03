2739264caa7d0a39b92ec74763baf92eb10aa17a
Kerillos Akram <kerillos.akram@gmail.com>
2021-10-18 23:49:40 +0200

    change project arch

---
+++ workspace/MiniProject3/Debug/main.d
@@ -0,0 +1,18 @@
+main.o main.o: ../main.c ../std_types.h ../common_macros.h \
+  ../lm35_sensor.h ../adc.h ../gpio.h ../lcd.h ../dc_motor.h ../pwm.h
+
+../std_types.h:
+
+../common_macros.h:
+
+../lm35_sensor.h:
+
+../adc.h:
+
+../gpio.h:
+
+../lcd.h:
+
+../dc_motor.h:
+
+../pwm.h:
